badc68dcc090ab5377516822544cb8f6455c7b61
Adelmo Menezes <adelmo.menezes2010@gmail.com>
2021-05-08 17:58:16 -0300

    corrige draw()

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -38,26 +38,48 @@ const tic_tac_toe = {
             this.turn_index = (this.turn_index === 0 ? 1 : 0);
         }
     },
+    player1: 'player 1',
+    player2: 'Player 2',
+    
+    // winn:{
+    //     qtd:0,
+    //     type=null,
+    // },
+
     container_element: null,
     gameover: false,
     winning_sequences: [
-        [0, 1, 2], [1, 2, 3], //LINHA 1 
-        [4, 5, 6], [5, 6, 7], // LINHA 2
-        [8, 9, 10], [9, 10, 11], // LINHA 3
-        [12, 13, 14], [13, 14, 15], // LINHA 4
+        [0, 1, 2],//LINHA 1       
+        [1, 2, 3], //LINHA 1 
 
-        [0, 4, 8], [4, 8, 12] // COLUNA 1
-        [1, 5, 9], [5, 9, 13] //COLUNA 2
-        [2, 6, 10], [6, 10, 14] // COLUNA 3
-        [3, 7, 11], [7, 11, 15] // COLUNA 4
+        [4, 5, 6],// LINHA 2
+        [5, 6, 7], // LINHA 2
 
-        [0, 5, 10], [5, 10, 15]
-        [4, 9, 14],
-        [8, 5, 2],
-        [12, 9, 6], [9, 6, 3],
+        [8, 9, 10], // LINHA 3
+        [9, 10, 11], // LINHA 3
+
+        [12, 13, 14], // LINHA 4
+        [13, 14, 15], // LINHA 4
+
+        [0, 4, 8],// COLUNA 1
+        [1, 5, 9],// COLUNA 2
+        [2, 6, 10],// COLUNA 3
+        [3, 7, 11],// COLUNA 4
+
+        [4, 8, 12], // COLUNA 1
+        [5, 9, 13], //COLUNA 2
+        [6, 10, 14], // COLUNA 3
+        [7, 11, 15], // COLUNA 4
+
+        [0, 5, 10],
         [1, 6, 11],
-        [13, 10, 7],
+        [2, 5, 8],
+        [3, 6, 9],
 
+        [4, 9, 14],
+        [5, 10, 15],
+        [6, 9, 12],
+        [7, 10, 13],
 
     ],
 
@@ -67,8 +89,8 @@ const tic_tac_toe = {
     },
 
     make_play(position) {
-
         if ((!!x4) && (this.gameover || this.boardx4[position] !== '')) return false;
+        
         else if ((!!x5) && (this.gameover || this.boardx5[position] !== '')) return false;
         else if ((!!x6) && (this.gameover || this.boardx6[position] !== '')) return false;
 
@@ -84,7 +106,10 @@ const tic_tac_toe = {
             this.game_is_over();
         }
         if (winning_sequences_index >= 0) {
+
             this.game_is_over();
+            
+            console.log(this.winning_sequences[winning_sequences_index])
             this.stylize_winner_sequence(this.winning_sequences[winning_sequences_index]);
         } else {
             this.symbols.change();
@@ -102,27 +127,33 @@ const tic_tac_toe = {
     },
 
     check_winning_sequences(symbol) {
-
         for (i in this.winning_sequences) {
-console.log(i)
             if (!!x4) {
-                if (this.boardx4[this.winning_sequences[i][0]] == symbol &&
+                if (
+                    this.boardx4[this.winning_sequences[i][0]] == symbol &&
                     this.boardx4[this.winning_sequences[i][1]] == symbol &&
-                    this.boardx4[this.winning_sequences[i][2]] == symbol &&
-                    this.boardx4[this.winning_sequences[i][3]] == symbol) {
-                    alert('winning sequences INDEX:' + i);
+                    this.boardx4[this.winning_sequences[i][2]] == symbol
+                ) {
+
+
+                    if (symbol == 'O') {
+                        alert(`${this.player1} - winning sequences INDEX: ${i}`);
+                    }
+                    if (symbol == 'X') {
+                        alert(`${this.player2} - winning sequences INDEX: ${i}`);
+                    }
+
                     return i;
+
                 }
             }
-
-
         };
         return -1;
     },
 
     game_is_over() {
         this.gameover = true;
-        alert('GAME OVER');
+        // alert(`GAME OVER - Jogador: ${this.symbol}`);
     },
 
     is_game_over() {
