9befe6ef60695327c8cd2800b6ca53ea3bdd816f
Adelmo Menezes <adelmo.menezes2010@gmail.com>
2021-05-17 14:36:20 -0300

    refatora funcao que valida sequencia de vitorias

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -23,7 +23,7 @@ function matriz4x4() {
     matriz.push([{ 0: null }, { 1: null }, { 2: null }, { 3: null }]);
     matriz.push([{ 0: null }, { 1: null }, { 2: null }, { 3: null }]);
     document.querySelector('.game').classList.add('x4');
-    document.querySelector('.game').classList.remove('x5','x6')
+    document.querySelector('.game').classList.remove('x5', 'x6')
     tic_tac_toe.start();
 }
 
@@ -34,7 +34,7 @@ function matriz5x5() {
     });
     matriz.push([{ 0: null }, { 1: null }, { 2: null }, { 3: null }, { 4: null }]);
     document.querySelector('.game').classList.add('x5');
-    document.querySelector('.game').classList.remove('x4','x6')
+    document.querySelector('.game').classList.remove('x4', 'x6')
     tic_tac_toe.start();
 }
 
@@ -47,7 +47,7 @@ function matriz6x6() {
     matriz.push([{ 0: null }, { 1: null }, { 2: null }, { 3: null }, { 4: null }, { 5: null }]);
     matriz.push([{ 0: null }, { 1: null }, { 2: null }, { 3: null }, { 4: null }, { 5: null }]);
     document.querySelector('.game').classList.add('x6');
-    document.querySelector('.game').classList.remove('x5','x4')
+    document.querySelector('.game').classList.remove('x5', 'x4')
     tic_tac_toe.start();
 }
 
@@ -93,16 +93,19 @@ const tic_tac_toe = {
                     matriz[linha][coluna] = currentSymbol;
                     this.draw();
 
+                    // jogadores[currentSymbol].push(position)
+                    // console.log(jogadores[currentSymbol])
+
                     let sequenci_win = this.verificarJogada(parseInt(linha), parseInt(coluna));
-                    if (sequenci_win && jogadores[currentSymbol].length == 6) {
+                    if (sequenci_win && jogadores[currentSymbol].length >= 5) {
                         console.table(sequenci_win)
 
                         for (i in jogadores[currentSymbol]) {
                             this.addClassWinner(jogadores[currentSymbol][i])
                         }
-
+                    } else {
+                        this.symbols.change()
                     }
-                    this.symbols.change()
                 }
             }
         }
@@ -159,11 +162,26 @@ const tic_tac_toe = {
         else if (this.verificarJogadaMesmaDiagonalMeiofrente(linha, coluna)) { acertou = matriz; }
         else if (this.verificarJogadaMesmaDiagonalMeioAtras(linha, coluna)) { acertou = matriz; }
         else if (this.verificarJogadaMesmaDiagonalCimaFrenteBaixoAtras(linha, coluna)) { acertou = matriz }
+
+
+        if (this.verificaDiagonal_1(linha, coluna)) { acertou = matriz }
+        else if (this.verificaColunaLinha_1(linha, coluna)) { acertou = matriz }
         return acertou
     },
 
     condicaoJogadas(condicao1, condicao2, condicao3) {
         if ((condicao1 == condicao2) && (condicao1 == condicao3)) {
+            return true
+        } else {
+            return false
+        }
+    },
+
+    condicaoJogadas_5(condicao1, condicao2, condicao3, condicao4, condicao5) {
+        if ((condicao1 == condicao2) &&
+            (condicao1 == condicao3) &&
+            (condicao1 == condicao4) &&
+            (condicao1 == condicao5)) {
             return true
         } else {
             return false
@@ -193,10 +211,12 @@ const tic_tac_toe = {
         if (simboloFrente && simboloMeio && simboloAtras) {
             let acertou = this.condicaoJogadas(simboloAtras, simboloMeio, simboloFrente);
 
-            if ((acertou) &&
-                !this.pesquisaIdSimbolo(linha, coluna, this.selecioneId(linha, coluna)) &&
-                !this.pesquisaIdSimbolo(linha, coluna + 1, this.selecioneId(linha, coluna + 1)) &&
-                !this.pesquisaIdSimbolo(linha, coluna - 1, this.selecioneId(linha, coluna - 1))) {
+            if (
+                (acertou &&
+                    !this.pesquisaIdSimbolo(linha, coluna + 1, this.selecioneId(linha, coluna + 1))) ||
+
+                (acertou &&
+                    !this.pesquisaIdSimbolo(linha, coluna - 1, this.selecioneId(linha, coluna - 1)))) {
 
                 jogadores[matriz[linha][coluna]].push(this.selecioneId(linha, coluna))
                 jogadores[matriz[linha][coluna]].push(this.selecioneId(linha, coluna + 1))
@@ -216,10 +236,11 @@ const tic_tac_toe = {
         if (simboloInicio && simboloFrente1 && simboloFrente2) {
             let acertou = this.condicaoJogadas(simboloInicio, simboloFrente1, simboloFrente2)
 
-            if ((acertou) &&
-                !this.pesquisaIdSimbolo(linha, coluna, this.selecioneId(linha, coluna)) &&
-                !this.pesquisaIdSimbolo(linha, coluna + 1, this.selecioneId(linha, coluna + 1)) &&
-                !this.pesquisaIdSimbolo(linha, coluna + 2, this.selecioneId(linha, coluna + 2))) {
+            if (
+                (acertou &&
+                    !this.pesquisaIdSimbolo(linha, coluna + 1, this.selecioneId(linha, coluna + 1))) ||
+                (acertou &&
+                    !this.pesquisaIdSimbolo(linha, coluna + 2, this.selecioneId(linha, coluna + 2)))) {
 
                 jogadores[matriz[linha][coluna]].push(this.selecioneId(linha, coluna))
                 jogadores[matriz[linha][coluna]].push(this.selecioneId(linha, coluna + 1))
@@ -239,10 +260,10 @@ const tic_tac_toe = {
         if (simboloInicio && simboloAtras1 && simboloAtras2) {
             let acertou = this.condicaoJogadas(simboloInicio, simboloAtras1, simboloAtras2)
 
-            if ((acertou) &&
-                !this.pesquisaIdSimbolo(linha, coluna, this.selecioneId(linha, coluna)) &&
-                !this.pesquisaIdSimbolo(linha, coluna - 1, this.selecioneId(linha, coluna - 1)) &&
-                !this.pesquisaIdSimbolo(linha, coluna - 2, this.selecioneId(linha, coluna - 2))) {
+            if ((acertou &&
+                !this.pesquisaIdSimbolo(linha, coluna - 1, this.selecioneId(linha, coluna - 1))) ||
+                (acertou &&
+                    !this.pesquisaIdSimbolo(linha, coluna - 2, this.selecioneId(linha, coluna - 2)))) {
 
                 jogadores[matriz[linha][coluna]].push(this.selecioneId(linha, coluna))
                 jogadores[matriz[linha][coluna]].push(this.selecioneId(linha, coluna - 1))
@@ -265,10 +286,10 @@ const tic_tac_toe = {
             if (simboloInicio && simboloCima && simboloBaixo) {
                 let acertou = this.condicaoJogadas(simboloInicio, simboloCima, simboloBaixo)
 
-                if ((acertou) &&
-                    !this.pesquisaIdSimbolo(linha, coluna, this.selecioneId(linha, coluna)) &&
-                    !this.pesquisaIdSimbolo(linha - 1, coluna, this.selecioneId(linha - 1, coluna)) &&
-                    !this.pesquisaIdSimbolo(linha + 1, coluna, this.selecioneId(linha + 1, coluna))) {
+                if ((acertou &&
+                    !this.pesquisaIdSimbolo(linha - 1, coluna, this.selecioneId(linha - 1, coluna))) ||
+                    (acertou &&
+                        !this.pesquisaIdSimbolo(linha + 1, coluna, this.selecioneId(linha + 1, coluna)))) {
 
                     jogadores[matriz[linha][coluna]].push(this.selecioneId(linha, coluna))
                     jogadores[matriz[linha][coluna]].push(this.selecioneId(linha - 1, coluna))
@@ -290,10 +311,10 @@ const tic_tac_toe = {
             if (simboloInicio && simboloCima1 && simboloCima2) {
                 let acertou = this.condicaoJogadas(simboloInicio, simboloCima1, simboloCima2)
 
-                if ((acertou) &&
-                    !this.pesquisaIdSimbolo(linha, coluna, this.selecioneId(linha, coluna)) &&
-                    !this.pesquisaIdSimbolo(linha - 1, coluna, this.selecioneId(linha - 1, coluna)) &&
-                    !this.pesquisaIdSimbolo(linha - 2, coluna, this.selecioneId(linha - 2, coluna))) {
+                if ((acertou &&
+                    !this.pesquisaIdSimbolo(linha - 1, coluna, this.selecioneId(linha - 1, coluna))) ||
+                    (acertou &&
+                        !this.pesquisaIdSimbolo(linha - 2, coluna, this.selecioneId(linha - 2, coluna)))) {
 
                     jogadores[matriz[linha][coluna]].push(this.selecioneId(linha, coluna))
                     jogadores[matriz[linha][coluna]].push(this.selecioneId(linha - 1, coluna))
@@ -315,10 +336,10 @@ const tic_tac_toe = {
             if (simboloInicio && simboloBaixo1 && simboloBaixo2) {
                 let acertou = this.condicaoJogadas(simboloInicio, simboloBaixo1, simboloBaixo2)
 
-                if ((acertou) &&
-                    !this.pesquisaIdSimbolo(linha, coluna, this.selecioneId(linha, coluna)) &&
-                    !this.pesquisaIdSimbolo(linha + 1, coluna, this.selecioneId(linha + 1, coluna)) &&
-                    !this.pesquisaIdSimbolo(linha + 2, coluna, this.selecioneId(linha + 2, coluna))) {
+                if ((acertou &&
+                    !this.pesquisaIdSimbolo(linha + 1, coluna, this.selecioneId(linha + 1, coluna))) ||
+                    (acertou &&
+                        !this.pesquisaIdSimbolo(linha + 2, coluna, this.selecioneId(linha + 2, coluna)))) {
 
                     jogadores[matriz[linha][coluna]].push(this.selecioneId(linha, coluna))
                     jogadores[matriz[linha][coluna]].push(this.selecioneId(linha + 1, coluna))
@@ -340,10 +361,10 @@ const tic_tac_toe = {
             if (simboloInicio && simboloBaixoFrente1 && simboloBaixoFrente2) {
                 let acertou = this.condicaoJogadas(simboloInicio, simboloBaixoFrente1, simboloBaixoFrente2)
 
-                if ((acertou) &&
-                    !this.pesquisaIdSimbolo(linha, coluna, this.selecioneId(linha, coluna)) &&
-                    !this.pesquisaIdSimbolo(linha + 1, coluna + 1, this.selecioneId(linha + 1, coluna + 1)) &&
-                    !this.pesquisaIdSimbolo(linha + 2, coluna + 2, this.selecioneId(linha + 2, coluna + 2))) {
+                if ((acertou &&
+                    !this.pesquisaIdSimbolo(linha + 1, coluna + 1, this.selecioneId(linha + 1, coluna + 1))) ||
+                    (acertou &&
+                        !this.pesquisaIdSimbolo(linha + 2, coluna + 2, this.selecioneId(linha + 2, coluna + 2)))) {
 
                     jogadores[matriz[linha][coluna]].push(this.selecioneId(linha, coluna))
                     jogadores[matriz[linha][coluna]].push(this.selecioneId(linha + 1, coluna + 1))
@@ -365,10 +386,10 @@ const tic_tac_toe = {
             if (simboloInicio && simboloBaixoAtras1 && simboloBaixoAtras2) {
                 let acertou = this.condicaoJogadas(simboloInicio, simboloBaixoAtras1, simboloBaixoAtras2)
 
-                if ((acertou) &&
-                    !this.pesquisaIdSimbolo(linha, coluna, this.selecioneId(linha, coluna)) &&
-                    !this.pesquisaIdSimbolo(linha + 1, coluna - 1, this.selecioneId(linha + 1, coluna - 1)) &&
-                    !this.pesquisaIdSimbolo(linha + 2, coluna - 2, this.selecioneId(linha + 2, coluna - 2))) {
+                if ((acertou &&
+                    !this.pesquisaIdSimbolo(linha + 1, coluna - 1, this.selecioneId(linha + 1, coluna - 1))) ||
+                    (acertou &&
+                        !this.pesquisaIdSimbolo(linha + 2, coluna - 2, this.selecioneId(linha + 2, coluna - 2)))) {
 
                     jogadores[matriz[linha][coluna]].push(this.selecioneId(linha, coluna))
                     jogadores[matriz[linha][coluna]].push(this.selecioneId(linha + 1, coluna - 1))
@@ -390,10 +411,10 @@ const tic_tac_toe = {
             if (simboloInicio && simboloCimaAtras1 && simboloCimaAtras2) {
                 let acertou = this.condicaoJogadas(simboloInicio, simboloCimaAtras1, simboloCimaAtras2)
 
-                if ((acertou) &&
-                    !this.pesquisaIdSimbolo(linha, coluna, this.selecioneId(linha, coluna)) &&
-                    !this.pesquisaIdSimbolo(linha - 1, coluna - 1, this.selecioneId(linha - 1, coluna - 1)) &&
-                    !this.pesquisaIdSimbolo(linha - 2, coluna - 2, this.selecioneId(linha - 2, coluna - 2))) {
+                if ((acertou &&
+                    !this.pesquisaIdSimbolo(linha - 1, coluna - 1, this.selecioneId(linha - 1, coluna - 1))) ||
+                    (acertou &&
+                        !this.pesquisaIdSimbolo(linha - 2, coluna - 2, this.selecioneId(linha - 2, coluna - 2)))) {
 
                     jogadores[matriz[linha][coluna]].push(this.selecioneId(linha, coluna))
                     jogadores[matriz[linha][coluna]].push(this.selecioneId(linha - 1, coluna - 1))
@@ -415,10 +436,10 @@ const tic_tac_toe = {
             if (simboloInicio && simboloCimaFrente1 && simboloCimaFrente2) {
                 let acertou = this.condicaoJogadas(simboloInicio, simboloCimaFrente1, simboloCimaFrente2)
 
-                if ((acertou) &&
-                    !this.pesquisaIdSimbolo(linha, coluna, this.selecioneId(linha, coluna)) &&
-                    !this.pesquisaIdSimbolo(linha - 1, coluna + 1, this.selecioneId(linha - 1, coluna + 1)) &&
-                    !this.pesquisaIdSimbolo(linha - 2, coluna + 2, this.selecioneId(linha - 2, coluna + 2))) {
+                if ((acertou &&
+                    !this.pesquisaIdSimbolo(linha - 1, coluna + 1, this.selecioneId(linha - 1, coluna + 1))) ||
+                    (acertou &&
+                        !this.pesquisaIdSimbolo(linha - 2, coluna + 2, this.selecioneId(linha - 2, coluna + 2)))) {
 
                     jogadores[matriz[linha][coluna]].push(this.selecioneId(linha, coluna))
                     jogadores[matriz[linha][coluna]].push(this.selecioneId(linha - 1, coluna + 1))
@@ -439,10 +460,10 @@ const tic_tac_toe = {
             if (simboloInicio && simboloCimaAtras1 && simboloBaixoFrente2) {
                 let acertou = this.condicaoJogadas(simboloInicio, simboloCimaAtras1, simboloBaixoFrente2)
 
-                if ((acertou) &&
-                    !this.pesquisaIdSimbolo(linha, coluna, this.selecioneId(linha, coluna)) &&
-                    !this.pesquisaIdSimbolo(linha - 1, coluna - 1, this.selecioneId(linha - 1, coluna - 1)) &&
-                    !this.pesquisaIdSimbolo(linha + 1, coluna + 1, this.selecioneId(linha + 1, coluna + 1))) {
+                if ((acertou &&
+                    !this.pesquisaIdSimbolo(linha - 1, coluna - 1, this.selecioneId(linha - 1, coluna - 1))) ||
+                    (acertou &&
+                        !this.pesquisaIdSimbolo(linha + 1, coluna + 1, this.selecioneId(linha + 1, coluna + 1)))) {
 
                     jogadores[matriz[linha][coluna]].push(this.selecioneId(linha, coluna))
                     jogadores[matriz[linha][coluna]].push(this.selecioneId(linha - 1, coluna - 1))
@@ -463,10 +484,10 @@ const tic_tac_toe = {
             if (simboloInicio && simboloCimaAtras1 && simboloBaixoFrente2) {
                 let acertou = this.condicaoJogadas(simboloInicio, simboloCimaAtras1, simboloBaixoFrente2)
 
-                if ((acertou) &&
-                    !this.pesquisaIdSimbolo(linha, coluna, this.selecioneId(linha, coluna)) &&
-                    !this.pesquisaIdSimbolo(linha + 1, coluna - 1, this.selecioneId(linha + 1, coluna - 1)) &&
-                    !this.pesquisaIdSimbolo(linha - 1, coluna + 1, this.selecioneId(linha - 1, coluna + 1))) {
+                if ((acertou &&
+                    !this.pesquisaIdSimbolo(linha + 1, coluna - 1, this.selecioneId(linha + 1, coluna - 1))) ||
+                    (acertou &&
+                        !this.pesquisaIdSimbolo(linha - 1, coluna + 1, this.selecioneId(linha - 1, coluna + 1)))) {
                     jogadores[matriz[linha][coluna]].push(this.selecioneId(linha, coluna))
                     jogadores[matriz[linha][coluna]].push(this.selecioneId(linha + 1, coluna - 1))
                     jogadores[matriz[linha][coluna]].push(this.selecioneId(linha - 1, coluna + 1))
@@ -486,10 +507,10 @@ const tic_tac_toe = {
             if (simboloInicio && simboloCimaFrente1 && simboloBaixoAtras2) {
                 let acertou = this.condicaoJogadas(simboloInicio, simboloCimaFrente1, simboloBaixoAtras2)
 
-                if ((acertou) &&
-                    !this.pesquisaIdSimbolo(linha, coluna, this.selecioneId(linha, coluna)) &&
-                    !this.pesquisaIdSimbolo(linha - 1, coluna + 1, this.selecioneId(linha - 1, coluna + 1)) &&
-                    !this.pesquisaIdSimbolo(linha + 1, coluna - 1, this.selecioneId(linha + 1, coluna - 1))) {
+                if ((acertou &&
+                    !this.pesquisaIdSimbolo(linha - 1, coluna + 1, this.selecioneId(linha - 1, coluna + 1))) ||
+                    (acertou &&
+                        !this.pesquisaIdSimbolo(linha + 1, coluna - 1, this.selecioneId(linha + 1, coluna - 1)))) {
 
                     jogadores[matriz[linha][coluna]].push(this.selecioneId(linha, coluna))
                     jogadores[matriz[linha][coluna]].push(this.selecioneId(linha - 1, coluna + 1))
@@ -501,7 +522,65 @@ const tic_tac_toe = {
     },
 
 
+    verificaDiagonal_1(linha, coluna) {
+        if ((matriz[linha + 1] === undefined) || (matriz[linha - 1] === undefined)) {
+            return false
+        } else {
+            let condicao1 = matriz[linha][coluna];
+            let condicao2 = matriz[linha - 1][coluna - 1];
+            let condicao3 = matriz[linha - 1][coluna + 1];
 
+            let condicao4 = matriz[linha + 1][coluna - 1];
+            let condicao5 = matriz[linha + 1][coluna + 1];
+
+            if (condicao1 && condicao2 && condicao3 && condicao4 && condicao5) {
+                let acertou = this.condicaoJogadas_5(condicao1, condicao2, condicao3, condicao4, condicao5)
+
+                if (acertou) {
+                    jogadores[matriz[linha][coluna]] = []
+                    jogadores[matriz[linha][coluna]].push(this.selecioneId(linha, coluna))
+                    jogadores[matriz[linha][coluna]].push(this.selecioneId(linha - 1, coluna - 1))
+                    jogadores[matriz[linha][coluna]].push(this.selecioneId(linha - 1, coluna + 1))
+                    jogadores[matriz[linha][coluna]].push(this.selecioneId(linha + 1, coluna - 1))
+                    jogadores[matriz[linha][coluna]].push(this.selecioneId(linha + 1, coluna + 1))
+                    console.log(jogadores[matriz[linha][coluna]])
+
+                }
+                return acertou
+            }
+        }
+
+    },
+
+    verificaColunaLinha_1(linha, coluna) {
+
+
+        if ((matriz[linha + 1] === undefined) || (matriz[linha - 1] === undefined)) {
+            return false
+        } else {
+            let condicao1 = matriz[linha][coluna];
+            let condicao2 = matriz[linha][coluna - 1];
+            let condicao3 = matriz[linha][coluna + 1];
+            let condicao4 = matriz[linha - 1][coluna];
+            let condicao5 = matriz[linha + 1][coluna];
+
+            if (condicao1 && condicao2 && condicao3 && condicao4 && condicao5) {
+                let acertou = this.condicaoJogadas_5(condicao1, condicao2, condicao3, condicao4, condicao5)
+
+                if (acertou) {
+                    jogadores[matriz[linha][coluna]] = []
+                    jogadores[matriz[linha][coluna]].push(this.selecioneId(linha, coluna))
+                    jogadores[matriz[linha][coluna]].push(this.selecioneId(linha, coluna - 1))
+                    jogadores[matriz[linha][coluna]].push(this.selecioneId(linha, coluna + 1))
+                    jogadores[matriz[linha][coluna]].push(this.selecioneId(linha - 1, coluna))
+                    jogadores[matriz[linha][coluna]].push(this.selecioneId(linha + 1, coluna))
+                    console.log(jogadores[matriz[linha][coluna]])
+
+                }
+                return acertou
+            }
+        }
+    },
 
 
 
